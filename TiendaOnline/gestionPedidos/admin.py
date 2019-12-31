9b0066647a8a04c87174fd446ad6d978f18e52dd
Douglas Ardila Garcés <dag1702@hotmail.com>
2019-12-30 20:03:52 -0500

    admin

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Clientes, Articulos, Pedidos
+
+admin.site.register(Clientes)
+admin.site.register(Articulos)
+admin.site.register(Pedidos)
