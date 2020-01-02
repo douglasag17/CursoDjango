ce23ca728a878da92bb52e929c4d2f3b89f32e3e
Douglas Ardila Garcés <dag1702@hotmail.com>
2020-01-02 16:40:32 -0500

    Update admin.py

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -6,6 +6,14 @@ from gestionPedidos.models import Cliente, Articulo, Pedido
 class ClientesAdmin(admin.ModelAdmin):
     list_display = ("nombre", "direccion", "email")
 
+class ArticulosAdmin(admin.ModelAdmin):
+    list_filter = ("seccion", )
+
+class PedidosAdmin(admin.ModelAdmin):
+    list_display = ("numero", "fecha")
+    list_filter = ("fecha", )
+    date_hierarchy = "fecha"
+
 admin.site.register(Cliente, ClientesAdmin)
-admin.site.register(Articulo)
-admin.site.register(Pedido)
+admin.site.register(Articulo, ArticulosAdmin)
+admin.site.register(Pedido, PedidosAdmin)
